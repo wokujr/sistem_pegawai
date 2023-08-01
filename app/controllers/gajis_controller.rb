ef084e80fac35a55bd11c4801e2904dec1b0a50d
wokujr <wokujr@gmail.com>
2023-08-01 17:10:01 +0700

    create delete and add function

--- app/controllers/gajis_controller.rb
+++ app/controllers/gajis_controller.rb
@@ -1,5 +1,23 @@
 class GajisController < ApplicationController
-  def index
-
+  def new
+    @karyawan = Karyawan.find(params[:karyawan_id])
+    @gaji = Gaji.new
   end
+  def create
+    @karyawan = Karyawan.find(params[:karyawan_id])
+    @gaji = @karyawan.build_gaji(gaji_karyawan)
+
+    if @gaji.save
+      redirect_to karyawan_path(@karyawan), notice:"Berhasil memasukan Gaji"
+    else
+      render new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def gaji_karyawan
+    params.require(:gaji).permit(:karyawan_id, :kode_gaji)
+  end
+
 end
--- app/controllers/karyawans_controller.rb
+++ app/controllers/karyawans_controller.rb
@@ -9,6 +9,28 @@ class KaryawansController < ApplicationController
   end
 
   def new
+    @karyawan = Karyawan.new
   end
 
+  def create
+    @karyawan = Karyawan.new(employer_params)
+    if @karyawan.save
+      redirect_to karyawans_path, notice: "Berhasil menambahkan karyawan baru"
+
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @karyawan = Karyawan.find(params[:id])
+    @karyawan.destroy
+    redirect_to karyawans_path, notice: "Berhasil menghapus Kayawan"
+  end
+
+  private
+
+  def employer_params
+    params.require(:karyawan).permit(:nama, :alamat, :usia, :jabatan)
+  end
 end
